aac45f3582c538d363d6a310cbe40a6808720c0b
Ayush Gupta <guptaaayush5678@gmail.com>
2026-05-09 17:58:16 +0530

    cors errors

--- src/app.js
+++ src/app.js
@@ -5,9 +5,10 @@ import cors from "cors"
 const app = express()
 
 app.use(cors({
-    origin : process.env.CORS_ORIGIN,
+    origin : "*",
     credentials : true
 }))
+
 
 
 app.use(express.json({limit:"16kb"}))
